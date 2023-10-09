f6653eeb96dca2f9e4d83381bfb2956ba8a3ec28
aimarsg <aimar.s.737@gmail.com>
2023-10-09 23:27:39 +0200

    fichero clustering

---
+++ clustering.py
@@ -0,0 +1,37 @@
+clusters = {}
+distancias = {}
+
+def calcularDistancias():
+    '''
+    calcular las distancias
+    :return:
+    '''
+    pass
+
+
+def agrupar():
+    '''
+    coger las distancias mas pequeñas y juntarlas en clusters
+    :return:
+    '''
+    pass
+
+
+def actualizarDistacias():
+    '''
+    eliminar las distancias que sobran y recalcular las de los
+    clusters que han cambiado
+    :return:
+    '''
+    pass
+
+
+def main():
+    # 1. limpiar datos
+    # 2. vecorizacion
+    # 3. clustering
+    pass
+
+
+if __name__ == "__main__":
+    main()
