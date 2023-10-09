2acbc30f6beb1c436c18a6072c30fa8ffb80f68d
aimarsg <aimar.s.737@gmail.com>
2023-10-10 00:35:08 +0200

    limpiar dataset hecho

--- limpiarDatos.py
+++ limpiarDatos.py
@@ -1,15 +1,310 @@
 import csv
 import sys
 import pandas as pd  # Importa Pandas
-
+from spellchecker import SpellChecker
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from textblob import TextBlob
 
+# source https://www.kaggle.com/code/sudalairajkumar/getting-started-with-text-preprocessing
 
+# Thanks : https://github.com/NeelShah18/emot/blob/master/emot/emo_unicode.py
+EMOTICONS = {
+    u":‑\)":"Happy face or smiley",
+    u":\)":"Happy face or smiley",
+    u":-\]":"Happy face or smiley",
+    u":\]":"Happy face or smiley",
+    u":-3":"Happy face smiley",
+    u":3":"Happy face smiley",
+    u":->":"Happy face smiley",
+    u":>":"Happy face smiley",
+    u"8-\)":"Happy face smiley",
+    u":o\)":"Happy face smiley",
+    u":-\}":"Happy face smiley",
+    u":\}":"Happy face smiley",
+    u":-\)":"Happy face smiley",
+    u":c\)":"Happy face smiley",
+    u":\^\)":"Happy face smiley",
+    u"=\]":"Happy face smiley",
+    u"=\)":"Happy face smiley",
+    u":‑D":"Laughing, big grin or laugh with glasses",
+    u":D":"Laughing, big grin or laugh with glasses",
+    u"8‑D":"Laughing, big grin or laugh with glasses",
+    u"8D":"Laughing, big grin or laugh with glasses",
+    u"X‑D":"Laughing, big grin or laugh with glasses",
+    u"XD":"Laughing, big grin or laugh with glasses",
+    u"=D":"Laughing, big grin or laugh with glasses",
+    u"=3":"Laughing, big grin or laugh with glasses",
+    u"B\^D":"Laughing, big grin or laugh with glasses",
+    u":-\)\)":"Very happy",
+    u":‑\(":"Frown, sad, andry or pouting",
+    u":-\(":"Frown, sad, andry or pouting",
+    u":\(":"Frown, sad, andry or pouting",
+    u":‑c":"Frown, sad, andry or pouting",
+    u":c":"Frown, sad, andry or pouting",
+    u":‑<":"Frown, sad, andry or pouting",
+    u":<":"Frown, sad, andry or pouting",
+    u":‑\[":"Frown, sad, andry or pouting",
+    u":\[":"Frown, sad, andry or pouting",
+    u":-\|\|":"Frown, sad, andry or pouting",
+    u">:\[":"Frown, sad, andry or pouting",
+    u":\{":"Frown, sad, andry or pouting",
+    u":@":"Frown, sad, andry or pouting",
+    u">:\(":"Frown, sad, andry or pouting",
+    u":'‑\(":"Crying",
+    u":'\(":"Crying",
+    u":'‑\)":"Tears of happiness",
+    u":'\)":"Tears of happiness",
+    u"D‑':":"Horror",
+    u"D:<":"Disgust",
+    u"D:":"Sadness",
+    u"D8":"Great dismay",
+    u"D;":"Great dismay",
+    u"D=":"Great dismay",
+    u"DX":"Great dismay",
+    u":‑O":"Surprise",
+    u":O":"Surprise",
+    u":‑o":"Surprise",
+    u":o":"Surprise",
+    u":-0":"Shock",
+    u"8‑0":"Yawn",
+    u">:O":"Yawn",
+    u":-\*":"Kiss",
+    u":\*":"Kiss",
+    u":X":"Kiss",
+    u";‑\)":"Wink or smirk",
+    u";\)":"Wink or smirk",
+    u"\*-\)":"Wink or smirk",
+    u"\*\)":"Wink or smirk",
+    u";‑\]":"Wink or smirk",
+    u";\]":"Wink or smirk",
+    u";\^\)":"Wink or smirk",
+    u":‑,":"Wink or smirk",
+    u";D":"Wink or smirk",
+    u":‑P":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u":P":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u"X‑P":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u"XP":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u":‑Þ":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u":Þ":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u":b":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u"d:":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u"=p":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u">:P":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u":‑/":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":/":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":-[.]":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u">:[(\\\)]":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u">:/":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":[(\\\)]":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u"=/":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u"=[(\\\)]":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":L":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u"=L":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":S":"Skeptical, annoyed, undecided, uneasy or hesitant",
+    u":‑\|":"Straight face",
+    u":\|":"Straight face",
+    u":$":"Embarrassed or blushing",
+    u":‑x":"Sealed lips or wearing braces or tongue-tied",
+    u":x":"Sealed lips or wearing braces or tongue-tied",
+    u":‑#":"Sealed lips or wearing braces or tongue-tied",
+    u":#":"Sealed lips or wearing braces or tongue-tied",
+    u":‑&":"Sealed lips or wearing braces or tongue-tied",
+    u":&":"Sealed lips or wearing braces or tongue-tied",
+    u"O:‑\)":"Angel, saint or innocent",
+    u"O:\)":"Angel, saint or innocent",
+    u"0:‑3":"Angel, saint or innocent",
+    u"0:3":"Angel, saint or innocent",
+    u"0:‑\)":"Angel, saint or innocent",
+    u"0:\)":"Angel, saint or innocent",
+    u":‑b":"Tongue sticking out, cheeky, playful or blowing a raspberry",
+    u"0;\^\)":"Angel, saint or innocent",
+    u">:‑\)":"Evil or devilish",
+    u">:\)":"Evil or devilish",
+    u"\}:‑\)":"Evil or devilish",
+    u"\}:\)":"Evil or devilish",
+    u"3:‑\)":"Evil or devilish",
+    u"3:\)":"Evil or devilish",
+    u">;\)":"Evil or devilish",
+    u"\|;‑\)":"Cool",
+    u"\|‑O":"Bored",
+    u":‑J":"Tongue-in-cheek",
+    u"#‑\)":"Party all night",
+    u"%‑\)":"Drunk or confused",
+    u"%\)":"Drunk or confused",
+    u":-###..":"Being sick",
+    u":###..":"Being sick",
+    u"<:‑\|":"Dump",
+    u"\(>_<\)":"Troubled",
+    u"\(>_<\)>":"Troubled",
+    u"\(';'\)":"Baby",
+    u"\(\^\^>``":"Nervous or Embarrassed or Troubled or Shy or Sweat drop",
+    u"\(\^_\^;\)":"Nervous or Embarrassed or Troubled or Shy or Sweat drop",
+    u"\(-_-;\)":"Nervous or Embarrassed or Troubled or Shy or Sweat drop",
+    u"\(~_~;\) \(・\.・;\)":"Nervous or Embarrassed or Troubled or Shy or Sweat drop",
+    u"\(-_-\)zzz":"Sleeping",
+    u"\(\^_-\)":"Wink",
+    u"\(\(\+_\+\)\)":"Confused",
+    u"\(\+o\+\)":"Confused",
+    u"\(o\|o\)":"Ultraman",
+    u"\^_\^":"Joyful",
+    u"\(\^_\^\)/":"Joyful",
+    u"\(\^O\^\)／":"Joyful",
+    u"\(\^o\^\)／":"Joyful",
+    u"\(__\)":"Kowtow as a sign of respect, or dogeza for apology",
+    u"_\(\._\.\)_":"Kowtow as a sign of respect, or dogeza for apology",
+    u"<\(_ _\)>":"Kowtow as a sign of respect, or dogeza for apology",
+    u"<m\(__\)m>":"Kowtow as a sign of respect, or dogeza for apology",
+    u"m\(__\)m":"Kowtow as a sign of respect, or dogeza for apology",
+    u"m\(_ _\)m":"Kowtow as a sign of respect, or dogeza for apology",
+    u"\('_'\)":"Sad or Crying",
+    u"\(/_;\)":"Sad or Crying",
+    u"\(T_T\) \(;_;\)":"Sad or Crying",
+    u"\(;_;":"Sad of Crying",
+    u"\(;_:\)":"Sad or Crying",
+    u"\(;O;\)":"Sad or Crying",
+    u"\(:_;\)":"Sad or Crying",
+    u"\(ToT\)":"Sad or Crying",
+    u";_;":"Sad or Crying",
+    u";-;":"Sad or Crying",
+    u";n;":"Sad or Crying",
+    u";;":"Sad or Crying",
+    u"Q\.Q":"Sad or Crying",
+    u"T\.T":"Sad or Crying",
+    u"QQ":"Sad or Crying",
+    u"Q_Q":"Sad or Crying",
+    u"\(-\.-\)":"Shame",
+    u"\(-_-\)":"Shame",
+    u"\(一一\)":"Shame",
+    u"\(；一_一\)":"Shame",
+    u"\(=_=\)":"Tired",
+    u"\(=\^\·\^=\)":"cat",
+    u"\(=\^\·\·\^=\)":"cat",
+    u"=_\^=	":"cat",
+    u"\(\.\.\)":"Looking down",
+    u"\(\._\.\)":"Looking down",
+    u"\^m\^":"Giggling with hand covering mouth",
+    u"\(\・\・?":"Confusion",
+    u"\(?_?\)":"Confusion",
+    u">\^_\^<":"Normal Laugh",
+    u"<\^!\^>":"Normal Laugh",
+    u"\^/\^":"Normal Laugh",
+    u"\（\*\^_\^\*）" :"Normal Laugh",
+    u"\(\^<\^\) \(\^\.\^\)":"Normal Laugh",
+    u"\(^\^\)":"Normal Laugh",
+    u"\(\^\.\^\)":"Normal Laugh",
+    u"\(\^_\^\.\)":"Normal Laugh",
+    u"\(\^_\^\)":"Normal Laugh",
+    u"\(\^\^\)":"Normal Laugh",
+    u"\(\^J\^\)":"Normal Laugh",
+    u"\(\*\^\.\^\*\)":"Normal Laugh",
+    u"\(\^—\^\）":"Normal Laugh",
+    u"\(#\^\.\^#\)":"Normal Laugh",
+    u"\（\^—\^\）":"Waving",
+    u"\(;_;\)/~~~":"Waving",
+    u"\(\^\.\^\)/~~~":"Waving",
+    u"\(-_-\)/~~~ \($\·\·\)/~~~":"Waving",
+    u"\(T_T\)/~~~":"Waving",
+    u"\(ToT\)/~~~":"Waving",
+    u"\(\*\^0\^\*\)":"Excited",
+    u"\(\*_\*\)":"Amazed",
+    u"\(\*_\*;":"Amazed",
+    u"\(\+_\+\) \(@_@\)":"Amazed",
+    u"\(\*\^\^\)v":"Laughing,Cheerful",
+    u"\(\^_\^\)v":"Laughing,Cheerful",
+    u"\(\(d[-_-]b\)\)":"Headphones,Listening to music",
+    u'\(-"-\)':"Worried",
+    u"\(ーー;\)":"Worried",
+    u"\(\^0_0\^\)":"Eyeglasses",
+    u"\(\＾ｖ\＾\)":"Happy",
+    u"\(\＾ｕ\＾\)":"Happy",
+    u"\(\^\)o\(\^\)":"Happy",
+    u"\(\^O\^\)":"Happy",
+    u"\(\^o\^\)":"Happy",
+    u"\)\^o\^\(":"Happy",
+    u":O o_O":"Surprised",
+    u"o_0":"Surprised",
+    u"o\.O":"Surpised",
+    u"\(o\.o\)":"Surprised",
+    u"oO":"Surprised",
+    u"\(\*￣m￣\)":"Dissatisfied",
+    u"\(‘A`\)":"Snubbed or Deflated"
+}
+chat_words_str = """
+AFAIK=As Far As I Know
+AFK=Away From Keyboard
+ASAP=As Soon As Possible
+ATK=At The Keyboard
+ATM=At The Moment
+A3=Anytime, Anywhere, Anyplace
+BAK=Back At Keyboard
+BBL=Be Back Later
+BBS=Be Back Soon
+BFN=Bye For Now
+B4N=Bye For Now
+BRB=Be Right Back
+BRT=Be Right There
+BTW=By The Way
+B4=Before
+B4N=Bye For Now
+CU=See You
+CUL8R=See You Later
+CYA=See You
+FAQ=Frequently Asked Questions
+FC=Fingers Crossed
+FWIW=For What It's Worth
+FYI=For Your Information
+GAL=Get A Life
+GG=Good Game
+GN=Good Night
+GMTA=Great Minds Think Alike
+GR8=Great!
+G9=Genius
+IC=I See
+ICQ=I Seek you (also a chat program)
+ILU=ILU: I Love You
+IMHO=In My Honest/Humble Opinion
+IMO=In My Opinion
+IOW=In Other Words
+IRL=In Real Life
+KISS=Keep It Simple, Stupid
+LDR=Long Distance Relationship
+LMAO=Laugh My A.. Off
+LOL=Laughing Out Loud
+LTNS=Long Time No See
+L8R=Later
+MTE=My Thoughts Exactly
+M8=Mate
+NRN=No Reply Necessary
+OIC=Oh I See
+PITA=Pain In The A..
+PRT=Party
+PRW=Parents Are Watching
+ROFL=Rolling On The Floor Laughing
+ROFLOL=Rolling On The Floor Laughing Out Loud
+ROTFLMAO=Rolling On The Floor Laughing My A.. Off
+SK8=Skate
+STATS=Your sex and age
+ASL=Age, Sex, Location
+THX=Thank You
+TTFN=Ta-Ta For Now!
+TTYL=Talk To You Later
+U=You
+U2=You Too
+U4E=Yours For Ever
+WB=Welcome Back
+WTF=What The F...
+WTG=Way To Go!
+WUF=Where Are You From?
+W8=Wait...
+7K=Sick:-D Laugher
+"""
 STOPWORDS = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+spell = SpellChecker()
 
 
 def remove_stopwords(text):
@@ -17,15 +312,56 @@ def remove_stopwords(text):
     return " ".join([word for word in str(text).split() if word not in STOPWORDS])
 
 
-stemmer = PorterStemmer()
 def stem_words(text):
     return " ".join([stemmer.stem(word) for word in text.split()])
+
 
 def convert_emoticons(text):
     for emot in EMOTICONS:
         text = re.sub(u'('+emot+')', "_".join(EMOTICONS[emot].replace(",","").split()), text)
     return text
 
+
+chat_words_map_dict = {}
+chat_words_list = []
+for line in chat_words_str.split("\n"):
+    if line != "":
+        cw = line.split("=")[0]
+        cw_expanded = line.split("=")[1]
+        chat_words_list.append(cw)
+        chat_words_map_dict[cw] = cw_expanded
+chat_words_list = set(chat_words_list)
+
+
+def chat_words_conversion(text):
+    new_text = []
+    for w in text.split():
+        if w.upper() in chat_words_list:
+            new_text.append(chat_words_map_dict[w.upper()])
+        else:
+            new_text.append(w)
+    return " ".join(new_text)
+
+
+def correct_spellings(x):
+    for w in spell.unknown(x.split()):
+        if spell.correction(w) is not None:
+            x = re.sub(w, spell.correction(w), x)
+    return x
+
+'''
+corrected_text = []
+misspelled_words = spell.unknown(text.split())
+for word in text.split():
+    if word in misspelled_words:
+        corrected_word = spell.correction(word)
+        if corrected_word is not None:
+            corrected_text.append(corrected_word)
+    else:
+        corrected_text.append(word)
+return " ".join(corrected_text)'''
+
+'''
 def tokenize(text):
     min_length = 3
     words = map(lambda word: word.lower(), word_tokenize(text))
@@ -35,31 +371,39 @@ def tokenize(text):
 
     p = re.compile('[a-zA-Z]+')
     filtered_tokens = list(filter(lambda token: p.match(token) and len(token) >= min_length, tokens))
-    return filtered_tokens
+    return filtered_tokens'''
+
 
 def main():
-    if len(sys.argv) != 3:
-        print("Uso: python limpiarDatos.py entrada.csv salida.csv")
-        return
-
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
     try:
         # Cargar el archivo CSV en un DataFrame de Pandas
-        df = pd.read_csv(input_file, header=None)
-
+        df = pd.read_csv(input_file)
+        df['Post'] = df['Post'].str.strip("[]").str.strip("''")
 
         ####### PREPROCESADO #######
 
+        # chat words conversion
+        df["Post"] = df["Post"].apply(lambda text: chat_words_conversion(text))
+
+        # correct spelling
+        # df["Post"] = df["Post"].apply(lambda text: TextBlob(text).correct())
+
         # lowercasing
-        df[""] = df[""].str.lower()
+        df["Post"] = df["Post"].str.lower()
 
         # eliminar stopwords
-        df[""] = df[""].apply(lambda text: remove_stopwords(text))
+        df["Post"] = df["Post"].apply(lambda text: remove_stopwords(text))
 
         # stemming
-        df[""] = df[""].apply(lambda text: stem_words(text))
+        df["Post"] = df["Post"].apply(lambda text: stem_words(text))
+
+        # convertir emoticonos a palabras
+        df["Post"] = df["Post"].apply(lambda text: convert_emoticons(text))
+
+        print(df.head(5))
 
         ############################
 
@@ -70,5 +414,12 @@ def main():
     except FileNotFoundError:
         print(f"No se encontró el archivo '{input_file}'")
 
+
 if __name__ == "__main__":
-    main()
+
+    if len(sys.argv) != 3:
+        print("Uso: python limpiarDatos.py entrada.csv salida.csv")
+        exit(0)
+    else:
+        main()
+
