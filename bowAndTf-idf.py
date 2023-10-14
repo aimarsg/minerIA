bf91e87075ae95fa9adc69fceb4ac29a62fa91ee
HugoMedina0 <hugomedina0022@gmail.com>
2023-10-14 17:10:33 +0200

    clusters cercanos y fusionar clusters

--- bowAndTf-idf.py
+++ bowAndTf-idf.py
@@ -7,6 +7,7 @@ from gensim.corpora import Dictionary
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.matutils import sparse2full
 
 def main():
 
@@ -19,29 +20,33 @@ def main():
                 return str(x)
         else:
             return str(x)
-    
-    ml_dataset = pd.read_csv('output.csv')
+
+    ml_dataset = pd.read_csv('output.csv', header=None, names=['User', 'Text', 'Label'])
 
     print(ml_dataset.head(5))
 
-    ml_dataset = ml_dataset[
-        ['User', 'Text', 'Label']]
+    ml_dataset = ml_dataset[['User', 'Text', 'Label']]
 
     text_features = ['Text']
-    
+
     for feature in text_features:
         ml_dataset[feature] = ml_dataset[feature].apply(coerce_to_unicode)
 
+    """
     dfSupportive = ml_dataset[ml_dataset['Label'] == 'Supportive']
     dfIdeation = ml_dataset[ml_dataset['Label'] == 'Ideation']
     dfBehavior = ml_dataset[ml_dataset['Label'] == 'Behavior']
     dfAttempt = ml_dataset[ml_dataset['Label'] == 'Attempt']
+    """
 
-    # GENERAR DOCUMENTOS
+    documentos = ml_dataset[ml_dataset['Text'].notna()]['Text'].tolist()
+
+    """# GENERAR DOCUMENTOS
     documentsSupportive = dfSupportive[dfSupportive['Text'].notna()]['Text'].tolist()
     documentsIdeation = dfIdeation[dfIdeation['Text'].notna()]['Text'].tolist()
     documentsBehavior = dfBehavior[dfBehavior['Text'].notna()]['Text'].tolist()
     documentsAttempt = dfAttempt[dfAttempt['Text'].notna()]['Text'].tolist()
+    """
 
     tokenizer = RegexpTokenizer(r'\w+')
 
@@ -54,44 +59,78 @@ def main():
             tokenized_documents.append(tokens)
         return tokenized_documents
 
-    documentsSupportive = tokenize_documents(documentsSupportive)
+    """documentsSupportive = tokenize_documents(documentsSupportive)
     documentsIdeation = tokenize_documents(documentsIdeation)
     documentsBehavior = tokenize_documents(documentsBehavior)
-    documentsAttempt = tokenize_documents(documentsAttempt)
-
+    documentsAttempt = tokenize_documents(documentsAttempt)"""
+    documentos = tokenize_documents(documentos)
     lemmatizer = WordNetLemmatizer()
+    documents = [[lemmatizer.lemmatize(token) for token in document] for document in documentos]
 
+    """
     documentsSupportive = [[lemmatizer.lemmatize(token) for token in document] for document in documentsSupportive]
     documentsIdeation = [[lemmatizer.lemmatize(token) for token in document] for document in documentsIdeation]
     documentsBehavior = [[lemmatizer.lemmatize(token) for token in document] for document in documentsBehavior]
     documentsAttempt = [[lemmatizer.lemmatize(token) for token in document] for document in documentsAttempt]
+"""
+    def bow_vectorize(document, dictionary):
+        bow_vector = dictionary.doc2bow(document)
+        dense_vector = sparse2full(bow_vector, len(dictionary))
+        return dense_vector
 
     #BOW
-    dictionary = Dictionary(documentsSupportive)
-    corpus = [dictionary.doc2bow(doc) for doc in documentsSupportive]
+    dictionary = Dictionary(documentos)
+    bow = [bow_vectorize(doc, dictionary) for doc in documentos]
     print('BOW --> ')
     print('Supportive')
     print('Number of unique tokens: %d' % len(dictionary))
-    print('Number of documents: %d' % len(corpus))
-
-    dictionary = Dictionary(documentsIdeation)
-    corpus = [dictionary.doc2bow(doc) for doc in documentsIdeation]
+    print('Number of documents: %d' % len(bow))
+    """
+    dictionary_ideation = Dictionary(documentsIdeation)
+    bow_ideation = [bow_vectorize(doc, dictionary_ideation) for doc in documentsIdeation]
     print('Ideation:')
-    print('Number of unique tokens: %d' % len(dictionary))
-    print('Number of documents: %d' % len(corpus))
+    print('Number of unique tokens: %d' % len(dictionary_ideation))
+    print('Number of documents: %d' % len(bow_ideation))
 
-    dictionary = Dictionary(documentsBehavior)
-    corpus = [dictionary.doc2bow(doc) for doc in documentsBehavior]
+    dictionary_behavior = Dictionary(documentsBehavior)
+    bow_behavior = [bow_vectorize(doc, dictionary_behavior) for doc in documentsBehavior]
     print('Behavior:')
-    print('Number of unique tokens: %d' % len(dictionary))
-    print('Number of documents: %d' % len(corpus))
+    print('Number of unique tokens: %d' % len(dictionary_behavior))
+    print('Number of documents: %d' % len(bow_behavior))
 
-    dictionary = Dictionary(documentsAttempt)
-    corpus = [dictionary.doc2bow(doc) for doc in documentsAttempt]
+    dictionary_attempt = Dictionary(documentsAttempt)
+    bow_attempt = [bow_vectorize(doc, dictionary_attempt) for doc in documentsAttempt]
     print('Attempt:')
-    print('Number of unique tokens: %d' % len(dictionary))
-    print('Number of documents: %d' % len(corpus))
+    print('Number of unique tokens: %d' % len(dictionary_attempt))
+    print('Number of documents: %d' % len(bow_attempt))
 
+    df_bow_supportive = pd.DataFrame(bow_supportive)
+    df_bow_ideation = pd.DataFrame(bow_ideation)
+    df_bow_behavior = pd.DataFrame(bow_behavior)
+    df_bow_attempt = pd.DataFrame(bow_attempt)
+    """
+    # Agregar columnas de usuario y etiqueta
+    df_bow = pd.DataFrame(bow)
+
+    df_bow['User'] = ml_dataset['User']
+    df_bow['Label'] = ml_dataset['Label']
+
+    """    
+    df_bow_ideation['User'] = dfIdeation['User']
+    df_bow_ideation['Label'] = 'Ideation'
+
+    df_bow_behavior['User'] = dfBehavior['User']
+    df_bow_behavior['Label'] = 'Behavior'
+
+    df_bow_attempt['User'] = dfAttempt['User']
+    df_bow_attempt['Label'] = 'Attempt'
+
+    # Concatenar todos los DataFrames
+    df_bow_all = pd.concat([df_bow_supportive, df_bow_ideation, df_bow_behavior, df_bow_attempt])
+    """
+    # Guardar en un archivo CSV
+    df_bow.to_csv('bow_results2.csv', index=False)
+"""
     #TF-IDF
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform([' '.join(doc) for doc in documentsSupportive])
@@ -117,6 +156,6 @@ def main():
     print('Attempt')
     print('Number of unique tokens: %d' % len(tfidf_vectorizer.get_feature_names()))
     print('Number of documents: %d' % len(tfidf_matrix.toarray()))
-
+"""
 if __name__ == "__main__":
     main()
--- clustering.py
+++ clustering.py
@@ -1,25 +1,102 @@
 import csv
 import sys
 import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+import math
+import matplotlib.pyplot as plt
 
-clusters = {}
+ClustersList = {}
 distancias = {}
 
 def leer_datos():
     input_file = sys.argv[1]
-    i = 0
-    with open(input_file, newline='') as File:
-        reader = csv.reader(File)
-        for row in reader:
-            lista = []
-            lista.append(list(row))
-            clusters[i]= lista
-            i+=1
+    df = pd.read_csv(input_file)
+    df = df.drop('User', axis=1)
+    df = df.drop('Label', axis=1)
+    return df.values.tolist()
 
+def reducir_dimensionalidad_pca(data):
+    # Realizar PCA para reducir la dimensionalidad a 500
+    pca = PCA(n_components=500)
+    df_reducido = pca.fit_transform(data)
+
+    return df_reducido
+
+def cluster_cercano(clusters):
+    distancia_minima = float('inf')
+    clusters_cercanos = (None, None)
+
+    for i in range(len(clusters)):
+        for j in range(i + 1, len(clusters)):
+            for punto1 in clusters[i]:
+                for punto2 in clusters[j]:
+                    distancia = euclidean_distance(punto1, punto2)
+                    if distancia < distancia_minima:
+                        distancia_minima = distancia
+                        clusters_cercanos = (i, j)
+
+    return clusters_cercanos
+
+def fusionar_clusters(clusters, clusters_cercanos):
+    nuevo_cluster_data = clusters[clusters_cercanos[0]] + clusters[clusters_cercanos[1]]
+    clusters.pop(clusters_cercanos[0])
+    clusters.pop(clusters_cercanos[1] - 1)
+    clusters.append(nuevo_cluster_data)
+
+def euclidean_distance(point1, point2):
+    return math.sqrt(sum((x - y) ** 2 for x, y in zip(point1, point2)))
+
+def actualizar_distancias(clusters, clusters_cercanos):
+    i, j = clusters_cercanos
+
+    # Calcular la distancia mínima entre el nuevo cluster y los clusters restantes
+    distancias_actualizadas = []
+
+    for k in range(len(clusters)):
+        if k != i and k != j:
+            distancia_minima = min(
+                distancias[(i, k)] if (i, k) in distancias else float('inf'),
+                distancias[(j, k)] if (j, k) in distancias else float('inf')
+            )
+
+            distancias_actualizadas.append(((i, k), distancia_minima))
+            distancias_actualizadas.append(((j, k), distancia_minima))
+
+    # Eliminar las distancias relacionadas con los clusters fusionados
+    for k in range(len(clusters)):
+        if (i, k) in distancias:
+            del distancias[(i, k)]
+        if (j, k) in distancias:
+            del distancias[(j, k)]
+
+    # Actualizar las distancias con las nuevas distancias mínimas
+    for distancia, valor in distancias_actualizadas:
+        distancias[distancia] = valor
+
+
+def cluster_jerarquico(data, umbral):
+    clusters = [[p] for p in data]
+    distancias_historia = []
+
+    while len(clusters) > 1:
+        clusters_cercanos = cluster_cercano(clusters)
+        distancia_minima = euclidean_distance(
+            clusters[clusters_cercanos[0]][0], clusters[clusters_cercanos[1]][0]
+        )
+
+        if distancia_minima > umbral:
+            break
+
+        distancias_historia.append((clusters_cercanos, distancia_minima))
+        fusionar_clusters(clusters, clusters_cercanos)
+        actualizar_distancias(clusters, clusters_cercanos)
+
+    return clusters, distancias_historia
 
 def calcular_distancia(instancia1, instancia2):
     # Calcula la distancia de Manhattan entre dos instancias
-    distancia = 0
+    distancia = float("inf")
     for i in range(len(instancia1)):
         distancia += abs(instancia1[i] - instancia2[i])
     return distancia
@@ -33,7 +110,7 @@ def calcular_distancia_average(cluster1, cluster2):
 
     return total_distancia / (len(cluster1) * len(cluster2))
 
-                              
+
 def calcular_distancias_clusters(instancias, clusters, distancias):
     # Calcular las distancias entre todos los clusters
     for i, cluster1 in clusters.items():
@@ -52,6 +129,7 @@ def agrupar(clusters, distancias):
     clusters[i].update(clusters[j])
     del clusters[j]
 
+"""
 def actualizar_distancias(clusters, distancias):
     # Eliminar las distancias que involucran al cluster fusionado y recalcular las distancias con el nuevo cluster
     cluster_indices = list(clusters.keys())
@@ -62,8 +140,9 @@ def actualizar_distancias(clusters, distancias):
             del distancias[(i, max(cluster_indices))]  # Eliminar distancias sobrantes
             distancia = calcular_distancia_average(clusters[i], clusters[max(cluster_indices)])
             distancias[(i, max(cluster_indices))] = distancia
-
+"""
 def main():
+    global clusters
     # 1. Limpieza de datos
     # 2. Vectorización
     # 3. Clustering jerárquico
@@ -89,5 +168,10 @@ if __name__ == "__main__":
         print("Uso: python clustering.py entrada.csv")
         exit(0)
     else:
-        leer_datos()
+        clusters = leer_datos()
+        clusters_dimensionados=reducir_dimensionalidad_pca(clusters)
+        print("Dimensionalidad reducida con PCA:", clusters_dimensionados.shape)
+        umbral_clusters = 5  # Ajusta este valor según tus necesidades
 
+        # Obtener clusters jerárquicos
+        clusters, resultado_clusters = cluster_jerarquico(clusters_dimensionados, umbral_clusters)
